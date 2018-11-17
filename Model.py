dff20706d170d92bb0050fc82ca248ada210ca0d
WesLSmith <smithwesl@live.com>
2018-11-17 09:26:15 -0500

    swtich to ols

--- Model.py
+++ Model.py
@@ -5,34 +5,53 @@ import statsmodels.api as sm
 
 
 if __name__ == "__main__":
-    team_data = pd.read_csv(r"S:\Analytics\Wes_S\csgo HTLV\teamstats.csv")
-    match_stats = pd.read_csv(r"S:\Analytics\Wes_S\csgo HTLV\matchstats.csv")
+    team_data = pd.read_csv(r"C:\Users\Wesley Smith\Desktop\csgobetting\hltv_pull\teamstats.csv")
+    match_stats = pd.read_csv(r"C:\Users\Wesley Smith\Desktop\csgobetting\hltv_pull\matchstats.csv")
 
 
     team_data
+    team_data["W/L"] = team_data["Win"]/team_data["Loss"]
 
     match_stats["win"] = match_stats["win"].apply(lambda x: [1 if x == "W" else 0][0])
+    match_stats["totalRounds"] = match_stats["roundsWon"] + match_stats["roundsLost"]
+
 
     joined_stats = pd.merge(match_stats, team_data, left_on = 'team', right_on="Name")
-
+    joined_stats = pd.merge(joined_stats, team_data[["Name","W/L","roundsPerMap"]], left_on = "opponent", right_on = "Name")
     joined_stats
 
     exogenous = joined_stats[[
-                            "Win",
-                            "Loss",
+                            "W/L_x",
+                            "W/L_y",
                             "kdr",
                             "maps_played",
-                            "roundsPerMap"
+                            "roundsPerMap_x",
+                            "roundsPerMap_y"
                             ]]
 
-    logit = sm.Logit(joined_stats["win"], exogenous)
-    result = logit.fit()
+    ols = sm.OLS(joined_stats["totalRounds"], exogenous)
+    result = ols.fit()
     print(result.summary())
 
-    joined_stats[joined_stats["team"] == "Fragsters"]
-    xnew = [54,
-            55,
-            .97,
-            109,
-            26.082569]
-    result.predict(xnew)
+    test = joined_stats[joined_stats["team"] == "GOSU"]
+    test2 = joined_stats[joined_stats["team"] == "CyberZen"]
+
+
+    test["1"] = 26.524
+    test["2"] = 1.805
+
+    xnew = test[[
+                "W/L_x",
+                "2",
+                # "W/L_y",
+                "kdr",
+                "maps_played",
+                "roundsPerMap_x",
+                # "roundsPerMap_y"
+                "1"
+                ]]
+    pred = result.predict(xnew)
+    pred
+    test["pred"] = pred
+    test["diff"] = test["totalRounds"] - test["pred"]
+    test["diff"].mean()
